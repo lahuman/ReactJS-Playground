d3a7fa3fc63c679733a243523f77cfc0f36b87b6
Daniel Lim <lahuman@daum.net>
2020-02-19 22:26:44 +0900

    feat : initialize reactjs playground

---
+++ README.md
@@ -0,0 +1,13 @@
+# React PlayGround
+
+## Project 생성
+
+> This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+```
+$ npx create-react-app react-playground
+```
+
+## [Material-UI 설정](https://github.com/lahuman/reactWithMaterialUI)
+
+## [react-router-dom 설정](https://reacttraining.com/react-router/web/guides/quick-start)
---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Album from './components/Album';
+
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Album</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Album />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
